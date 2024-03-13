b540388a8803fd793469f2db1bf08e8640ccd58c
Mahdi <mahdi.sundarani@gmail.com>
2024-03-13 17:43:31 +0530

    haptik-ai

--- src/components/ImpNumbers.jsx
+++ src/components/ImpNumbers.jsx
@@ -1,48 +1,64 @@
 import React from 'react'
 import Jio from '../assets/ImpNumbers/jio.webp'
-import Logo1 from '../assets/ImpNumbers/Netmeds_card-logo.webp'
-import Logo2 from '../assets/ImpNumbers/Starhublogo-card-logo.webp'
-import Logo3 from '../assets/ImpNumbers/paisabazaar-card-logo.webp'
+import Netmeds from '../assets/ImpNumbers/Netmeds_card-logo.webp'
+import StarHub from '../assets/ImpNumbers/Starhublogo-card-logo.webp'
+import PaisaBazaar from '../assets/ImpNumbers/paisabazaar-card-logo.webp'
 const ImpNumbers = () => {
-  return (
-    <div className='m-10'>
-        {/* heading  */}
-        <div className=''>
-            <h1 className='font-medium text-xl md:text-3xl text-center mb-4'>Haptik's Impact in Numbers</h1>
+    return (
+        <div className='m-10'>
+            {/* heading  */}
+            <div className=''>
+                <h1 className='font-medium text-xl md:text-3xl text-center mb-4'>Haptik's Impact in Numbers</h1>
+            </div>
+            {/* subheading */}
+            <div className='hidden md:flex justify-center'>
+                <p className='text-center'>The tangible outcomes achieved by our clients</p>
+            </div>
+            {/* cards */}
+            <div className='flex gap-6 sm:flex-wrap'>
+                {/* card 1 */}
+                <div className='shadow-xl hover:scale-105 duration-300 flex-1 pb-3'>
+                    <h1 className='text-blue-600 font-medium text-center text-5xl'>68%</h1>
+                    <p className='text-center pt-1 pb-5'>repeat rate on whatsapp commerce</p>
+                    <img className='p-1 h-auto md:h-[43px] w-[100%]  md:w-auto md:mx-auto my-auto' src={Jio} alt="" />
+                </div>
+                {/* card 2 */}
+                <div className='shadow-xl hover:scale-105 duration-300 flex-1 pb-3'>
+                    <h1 className='text-blue-600 font-medium text-center text-5xl'>3X</h1>
+                    <p className='text-center pt-1 pb-5'>increase in general leads</p>
+                    <img className='p-1 h-auto md:h-[43px] w-[100%]  md:w-auto md:mx-auto my-auto' src={PaisaBazaar} alt="" />
+                </div>
+                    {/* card 1 */}
+                    <div className='hidden sm:block shadow-xl hover:scale-105 duration-300 flex-1 pb-3'>
+                    <h1 className='text-blue-600 font-medium text-center text-5xl'>125%</h1>
+                    <p className='text-center pt-1 pb-5'>improvement in NPS</p>
+                    <img className='p-1 h-auto md:h-[43px] w-[100%]  md:w-auto md:mx-auto my-auto' src={StarHub} alt="" />
+                </div>
+                {/* card 2 */}
+                <div className='hidden sm:block shadow-xl hover:scale-105 duration-300 flex-1 pb-3'>
+                    <h1 className='text-blue-600 font-medium text-center text-5xl'>99%</h1>
+                    <p className='text-center pt-1 pb-5'>Improvement in response time</p>
+                    <img className='p-1 h-auto md:h-[43px] w-[100%]  md:w-auto md:mx-auto my-auto' src={Netmeds} alt="" />
+                </div>
+            </div>
+
+
+            <div className='sm:hidden flex gap-6 sm:flex-wrap pt-5 '>
+                {/* card 1 */}
+                <div className='shadow-xl hover:scale-105 duration-300 flex-1 pb-3'>
+                    <h1 className='text-blue-600 font-medium text-center text-5xl'>125%</h1>
+                    <p className='text-center pt-1 pb-5'>improvement in NPS</p>
+                    <img className='p-1 h-auto md:h-[43px] w-[100%]  md:w-auto md:mx-auto my-auto' src={StarHub} alt="" />
+                </div>
+                {/* card 2 */}
+                <div className='shadow-xl hover:scale-105 duration-300 flex-1 pb-3'>
+                    <h1 className='text-blue-600 font-medium text-center text-5xl'>99%</h1>
+                    <p className='text-center pt-1 pb-5'>Improvement in response time</p>
+                    <img className='p-1 h-auto md:h-[43px] w-[100%] md:w-auto' src={Netmeds} alt="" />
+                </div>
+            </div>
         </div>
-        {/* subheading */}
-        <div className='hidden md:flex justify-center'>
-            <p className='text-center'>The tangible outcomes achieved by our clients</p>
-        </div>
-        {/* cards */}
-        <div className='flex flex-wrap justify-between md:m-9 xl:w-[1000px] xl:mx-auto'>
-            {/* card 1 */}
-            <div className='shadow-xl  hover:scale-105 duration-300 rounded-xl mx-4 my-2 md:w-[44%] md:px-2 md:py-8 flex-block lg:w-auto xl:w-[20%]'>
-                <h1 className='text-4xl md:text-5xl text-blue-600 font-medium text-center'>68%</h1>
-                <p className='text-center mt-1 mb-10 flex md:block'>repeat rate on whatsapp</p>
-                <img className='h-24 md:h-10 mx-auto' src={Jio} alt="" />
-            </div>
-            {/* card 2 */}
-            <div className='shadow-xl  hover:scale-105 duration-300 rounded-xl mx-4 my-2 md:w-[44%] md:px-2 md:py-8 flex-block lg:w-auto xl:w-[20%]'>
-                <h1 className='text-4xl md:text-5xl text-blue-600 font-medium text-center'>3X</h1>
-                <p className='text-center mt-1 mb-10 flex md:block'>Increase in generated leads</p>
-                <img className='h-24 md:h-10 mx-auto' src={Logo1} alt="" />
-            </div>
-            {/* card 3 */}
-            <div className='shadow-xl  hover:scale-105 duration-300 rounded-xl mx-4 my-2 md:w-[44%] md:px-2 md:py-8 flex-block lg:w-auto xl:w-[20%]'>
-                <h1 className='text-4xl md:text-5xl text-blue-600 font-medium text-center'>125%</h1>
-                <p className='text-center mt-1 mb-10 flex md:block'>Improvment in NPS</p>
-                <img className='h-[75px] md:h-10 mx-auto' src={Logo2} alt="" />
-            </div>
-            {/* card 4 */}
-            <div className='shadow-xl  hover:scale-105 duration-300 rounded-xl mx-4 my-2 md:w-[44%] md:px-2 md:py-8 flex-block lg:w-auto xl:w-[20%]'>
-                <h1 className='text-4xl md:text-5xl text-blue-600 font-medium text-center'>99%</h1>
-                <p className='mt-1 mb-10 w-[90%] text-center md:block'>Improvement in time </p>
-                <img className='h-14 md:h-10 mx-2 md:mx-auto' src={Logo3} alt="" />
-            </div>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default ImpNumbers
