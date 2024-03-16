6ec94f081436e24fa07af19a1448e83dee468f54
Mahdi <mahdi.sundarani@gmail.com>
2024-03-16 14:15:28 +0530

    haptik-ai

--- src/schemas/index.jsx
+++ src/schemas/index.jsx
@@ -4,5 +4,14 @@ export const logInSchema =  Yup.object({
     name:Yup.string().min(2).max(25).required("Please enter your name"),
     email:Yup.string().email().required("Please enter your email"),
     password:Yup.string().min(6).required("Please enter your Password"),
-
 })
+
+export const signInSchema =  Yup.object({
+    Fname:Yup.string().min(2).max(25).required("Please enter your name"),
+    Lname:Yup.string().min(2).max(25).required("Please enter your name"),
+    email:Yup.string().email().required("Please enter your email"),
+    password:Yup.string().min(6).required("Please enter your Password"),
+    confirm_password: Yup.string()
+    .required()
+    .oneOf([Yup.ref("password"), null], "Password must match"),
+}) 
